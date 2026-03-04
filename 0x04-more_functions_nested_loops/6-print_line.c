9b58b15c4bc418c534c23bcacfb2773b1ab74dae
RefilweHope <Lekganyanerefilwe18@gmail.com>
2026-03-04 11:57:53 +0000

    Implement all required functions and programs for 0x04-more_functions_nested_loops (tasks 0-10)

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ *
+ * The line ends with a newline. If n is 0 or less, only a newline is printed.
+ */
+void print_line(int n)
+{
+    int i;
+
+    if (n > 0)
+    {
+        for (i = 0; i < n; i++)
+            _putchar('_');
+    }
+    _putchar('\n');
+}
