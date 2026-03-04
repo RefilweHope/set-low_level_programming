2679923c7c1de8369eebc865b44cf736edaca6d1
RefilweHope <Lekganyanerefilwe18@gmail.com>
2026-03-04 14:08:38 +0200

    Refactor FizzBuzz logic in main function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints the numbers from 1 to 100 with FizzBuzz substitutions
  *
@@ -8,18 +7,17 @@
 int main(void)
 {
     int i;
-
-    for (i = 1; i <= 100; i++)
+ for (i = 1; i <= 100; i++)
     {
-        if ((i % 3 == 0) && (i % 5 == 0))
+ if ((i % 3 == 0) && (i % 5 == 0))
             printf("FizzBuzz");
-        else if (i % 3 == 0)
+ else if (i % 3 == 0)
             printf("Fizz");
-        else if (i % 5 == 0)
+ else if (i % 5 == 0)
             printf("Buzz");
-        else
+ else
             printf("%d", i);
-        if (i < 100)
+ if (i < 100)
             putchar(' ');
     }
     putchar('\n');
