d8103885443a6d83949c8fa5f2736f4d3bb3724d
RefilweHope <Lekganyanerefilwe18@gmail.com>
2026-03-04 14:10:43 +0200

    Refactor FizzBuzz logic and formatting

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,18 +7,18 @@
 int main(void)
 {
     int i;
- for (i = 1; i <= 100; i++)
+for (i = 1; i <= 100; i++)
     {
- if ((i % 3 == 0) && (i % 5 == 0))
+if ((i % 3 == 0) && (i % 5 == 0))
             printf("FizzBuzz");
- else if (i % 3 == 0)
+else if (i % 3 == 0)
             printf("Fizz");
- else if (i % 5 == 0)
+else if (i % 5 == 0)
             printf("Buzz");
- else
+else
             printf("%d", i);
- if (i < 100)
-            putchar(' ');
+if (i < 100)
+           putchar(' ');
     }
     putchar('\n');
     return (0);
