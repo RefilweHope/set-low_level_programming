b6261f561ed56197495758960ef18d27748f0437
RefilweHope <Lekganyanerefilwe18@gmail.com>
2026-03-08 16:03:59 +0200

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_Strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string in place
+ * @s: pointer to the string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len;
+	int i;
+	char tmp;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = tmp;
+	}
+}
